a0e6edf36b61907dedf533ae1d34529e0c36c026
¨paalexan¨ <alexandremagteixeira@gmail.com>
2025-03-01 17:08:43 +0000

    norminette fix

--- src/utils/parsing/parsing.c
+++ src/utils/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: paalexan <paalexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:06:02 by paalexan          #+#    #+#             */
-/*   Updated: 2025/02/27 01:18:07 by paalexan         ###   ########.fr       */
+/*   Updated: 2025/03/01 17:08:01 by paalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ int	parse_args(t_stack **a, int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	if (ft_isstringempty(argv[1]) && !argv[2])
-		return (0);
+		return (-1);
 	i = 1;
 	while (i < argc)
 	{
